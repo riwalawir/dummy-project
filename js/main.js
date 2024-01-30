45c07ed28843c2b63611a472fa90717512279e9a
riwalawir <siriwalramdhani@gmail.com>
2024-01-30 19:55:00 +0700

    intial commit awal

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// TOGGLE & RESPONSIVE NAVIGATION
+const navSlide = () => { 
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+    
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
